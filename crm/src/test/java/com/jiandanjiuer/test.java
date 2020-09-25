ab4f615f5aee2e676967fd9525d388beac6b5335
jiandanjiuer <2424585654@qq.com>
2020-09-25 15:42:38 +0800

    测试1

--- crm/src/test/java/com/jiandanjiuer/test.java
+++ crm/src/test/java/com/jiandanjiuer/test.java
@@ -9,7 +9,8 @@ import org.junit.Test;
 public class test {
 
     @Test
-    public void string() {
+    public void
+    string() {
         String a = "2020-9-4";
         String b = "2020-9-1";
         System.out.println(a.compareTo(b) > -1);
